7faed55ec327f8941c59dbd6fde12a5396f3acda
David Zhao <dazhao@microsoft.com>
2016-06-17 16:29:23 -0700

    update

--- scripts/trigger-deploy.sh
+++ scripts/trigger-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -i -u $VSTS_USERNAME:$VSTS_PASSWORD -H "Content-Type: application/json" -X POST -d '{ "definition": { "id": 8 }, "sourceBranch": "refs/heads/master" }' https://azuredevex.visualstudio.com/DefaultCollection/Polyglot/_apis/build/builds?api-version=2.0
+curl -i -u $1 -H "Content-Type: application/json" -X POST -d '{ "definition": { "id": 8 }, "sourceBranch": "refs/heads/master" }' https://azuredevex.visualstudio.com/DefaultCollection/Polyglot/_apis/build/builds?api-version=2.0
