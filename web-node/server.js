c1d396018c2a49d04d19a9e2aeff1439a03d7dd9
Steve Lasker <StevenLasker@Hotmail.com>
2016-06-17 13:46:13 -0700

    fix for port mapping

--- web-node/server.js
+++ web-node/server.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 app.listen(port, function () {
     console.log("Listening on port " + port);
 });
