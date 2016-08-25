6d755ddb3fa6d3b598f4f5835b5653b3ead2fa26
Steve Lasker <StevenLasker@Hotmail.com>
2016-08-25 09:38:06 -0700

    Added comments

--- web-node/server.js
+++ web-node/server.js
@@ -7,19 +7,32 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(require("morgan")("dev"));
 
-var apiPort = process.env.APIPORT || 5000
-var apiURL = process.env.APIURL || "apidotnet"
-var requestApi = "http://"+apiURL+":"+apiPort+"/api/hello";
-app.get('/api', function (req, res) {
-    request(requestApi, function (error, response, body) {
-        res.send('From ' + requestApi + "-->" + body);
-    });    
-});
-
+// #######################################
+// Default content page
+// #######################################
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+// #######################################
+// Call the WebAPI for information
+// #######################################
+// build up the URL using environment info
+var apiPort = process.env.APIPORT || 80
+var apiURL = process.env.APIURL || "apidotnet"
+var requestApi = "http://"+apiURL+":"+apiPort+"/api/hello";
+
+app.get('/api', function (req, res) {
+    request(requestApi, function (error, response, body) {
+        res.send('From api-dotnet: ' + body);
+    });    
+});
+
+// #######################################
+// Start the web server
+// #######################################
+// Set the web server listenting port, based on an environment var
+// default to the local 3000 port
 var port = process.env.HTTPPORT || 3000;
 app.listen(port, function () {
     console.log("Listening on port " + port);
