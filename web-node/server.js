deec329d9f7e05f5372d3485c0929900abd43f97
Steve Lasker <StevenLasker@Hotmail.com>
2016-08-25 08:54:13 -0700

    Updted messaging content

--- web-node/server.js
+++ web-node/server.js
@@ -7,7 +7,18 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(require("morgan")("dev"));
 
-var apiPort = process.env.APIPORT || 5000
+// #######################################
+// Default content page
+// #######################################
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// #######################################
+// Call the WebAPI for information
+// #######################################
+// build up the URL using environment info
+var apiPort = process.env.APIPORT || 80
 var apiURL = process.env.APIURL || "apidotnet"
 var requestApi = "http://"+apiURL+":"+apiPort+"/api/hello";
 app.get('/api', function (req, res) {
@@ -16,10 +27,11 @@ app.get('/api', function (req, res) {
     });    
 });
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
-});
-
+// #######################################
+// Start the web server
+// #######################################
+// Set the web server listenting port, based on an environment var
+// default to the local 3000 port
 var port = process.env.HTTPPORT || 3000;
 app.listen(port, function () {
     console.log("Listening on port " + port);
